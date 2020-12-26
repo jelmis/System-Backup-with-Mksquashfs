d4d3abf91d754e1206ebbcb16f2fc4ddc5e475ce
jelmis <76618393+jelmis@users.noreply.github.com>
2020-12-26 14:59:19 +0100

    Update restore-script.sh

--- restore-script.sh
+++ restore-script.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 clear
-mkdir /mnt/backups
-mkdir /mnt/restore
+sudo mkdir /mnt/backups
+sudo mkdir /mnt/restore
 sudo fdisk -l | grep GiB
 echo Auf welche Festplatte soll zurück geschrieben werden '(sda, sdb oder sdc,...)'
 read volume
 sudo sfdisk --force /dev/$volume <secured-partition-table.mbr
-umount $volume'1'
-umount $volume'2'
+sudo umount $volume'1'
+sudo umount $volume'2'
 uuid=`cat uuid.txt`
-mkfs.fat /dev/$volume'1'
-mkfs.ext4 -F -U $uuid /dev/$volume'2'
+sudo mkfs.fat /dev/$volume'1'
+sudo mkfs.ext4 -F -U $uuid /dev/$volume'2'
 
-mount /dev/$volume'2' /mnt/restore/
-mkdir /mnt/restore/boot
-mkdir /mnt/restore/boot/efi
-mount /dev/$volume'1' /mnt/restore/boot/efi
-mount image.img /mnt/backups/
-mkdir /mnt/restore/dev
-mkdir /mnt/restore/proc
-mkdir /mnt/restore/sys
-mkdir /mnt/restore/media
-mkdir /mnt/restore/mnt
-mkdir /mnt/restore/run
+sudo mount /dev/$volume'2' /mnt/restore/
+sudo mkdir /mnt/restore/boot
+sudo mkdir /mnt/restore/boot/efi
+sudo mount /dev/$volume'1' /mnt/restore/boot/efi
+sudo mount image.img /mnt/backups/
+sudo mkdir /mnt/restore/dev
+sudo mkdir /mnt/restore/proc
+sudo mkdir /mnt/restore/sys
+sudo mkdir /mnt/restore/media
+sudo mkdir /mnt/restore/mnt
+sudo mkdir /mnt/restore/run
 cd /mnt/backups
